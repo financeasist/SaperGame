19f7e7a6fd947ec05df5f89839885d1e561b5d17
Roman Grupskyi <financeasist@gmail.com>
2017-02-21 17:01:00 +0200

    refacored predicat1

--- src/main/java/controller/BotController.java
+++ src/main/java/controller/BotController.java
@@ -49,9 +49,11 @@ public class BotController {
 		int bombAround = cell.getBombArround();
 		log.debug("doRandomClick return  Cell[" + x + "][" + y + "]" + "bombArround = " + bombAround);
 		resetProbabilities();
-		doNextClick();
+		Cell clickedCell = doNextClick();
+		log.debug("doNextClick() returns Cell[" + clickedCell.getPositionX() + "][" + clickedCell.getPositionY()
+				+ "] value = " + clickedCell.getBombArround());
 
-		ArrayList<Cell> list = (ArrayList<Cell>) findNeighbours(cell);
+		// ArrayList<Cell> list = (ArrayList<Cell>) findNeighbours(cell);
 
 	}
 
@@ -59,10 +61,146 @@ public class BotController {
 	 * fills array probabilities by zero
 	 */
 	public void resetProbabilities() {
-		for (Float[] row : probabilities) {
-			for (Float value : row) {
-				value = 0f;
+		for (int i = 0; i < board.getBoardWidth(); i++) {
+			for (int j = 0; j < board.getBoardHeight(); j++) {
+
+				probabilities[i][j] = 0f;
 			}
+		}
+	}
+
+	/**
+	 * does random click on board
+	 * 
+	 * @return clicked cell
+	 */
+	public Cell doRandomClick() {
+		Random random = new Random();
+		int randomY = random.nextInt(board.getBoardWidth());
+		int randomX = random.nextInt(board.getBoardHeight());
+		Cell cell = cells[randomX][randomY];
+		Cell openedCell = cell.doOpen();
+		return openedCell;
+	}
+
+	public Cell doNextClick() {
+		log.debug("satrted doNextClick()");
+		setProbabilityIntoClosedCells();
+		setBotFlags();
+		Cell newClikedCell = findAndClickOnCellWithLatestProb();
+		return newClikedCell;
+	}
+
+	private Cell findAndClickOnCellWithLatestProb() {
+		Cell cellWithLessProbabiliti = findCellWithLessProbabiliti(findTheLeastProbability());
+		cellWithLessProbabiliti.doOpen();
+		// Cell clickedCell = null;
+		// for (Cell[] row : cells) {
+		// for (Cell cell : row) {
+		// if (!cell.isNeedsToOpen()) {
+		// if (cell.equals(cellWithLessProbabiliti)) {
+		// clickedCell = cell.doOpen();
+		// // }else{
+		// // clickedCell = doRandomClick();
+		// // }
+		//
+		// }
+		// }
+		// }
+		// }
+		return cellWithLessProbabiliti;
+	}
+
+	private void setProbabilityIntoClosedCells() {
+		for (Cell[] row : cells) {
+			for (Cell cell : row) {
+				if (!cell.isNeedsToOpen()) {
+					setProbability(cell);
+				}
+			}
+		}
+	}
+
+	private void setBotFlags() {
+		log.debug("started setBotFlags()");
+		for (int i = 0; i < board.getBoardWidth(); i++) {
+			for (int j = 0; j < board.getBoardHeight(); j++) {
+				if (probabilities[i][j] != null) {
+					if (probabilities[i][j] == 1f) {
+						cells[i][j].doFlag();
+					}
+
+				}
+			}
+		}
+	}
+
+	private Cell findCellWithLessProbabiliti(Float min) {
+		Cell cell = cells[0][0];
+		for (int i = 0; i < board.getBoardWidth(); i++) {
+			for (int j = 0; j < board.getBoardHeight(); j++) {
+				if (probabilities[i][j] == min) {
+					cell = cells[i][j];
+				}
+
+			}
+		}
+
+		return cell;
+	}
+
+	private Float findTheLeastProbability() {
+		Float min = 0f;
+		for (int i = 0; i < board.getBoardWidth(); i++) {
+			for (int j = 0; j < board.getBoardHeight(); j++) {
+				if (probabilities[i][j] != null) {
+					if (probabilities[i][j] < min) {
+						min = probabilities[i][j];
+					}
+
+				}
+			}
+		}
+		return min;
+	}
+
+	public boolean isPredicat1(Cell cell) {
+		boolean predicat1 = false;
+		int x = cell.getPositionX();
+		int y = cell.getPositionY();
+		boolean bottomRightCorner = false;
+		boolean upperRightСorner = false;
+		boolean upperLeftCorner = false;
+		boolean bottomLeftCorner = false;
+		if ((x + 1) < board.getBoardWidth() && (y + 1) < board.getBoardHeight()) {
+			// && (x - 1) > 0 && (y - 1) > 0) {
+			if ((y - 1) > 0) {
+				bottomRightCorner = (cells[x + 1][y].getBombArround() > 0) && (cells[x + 1][y - 1].getBombArround() > 0)
+						&& cells[x][y - 1].getBombArround() > 0;
+			}
+			upperRightСorner = cells[x + 1][y].getBombArround() > 0 && cells[x + 1][y + 1].getBombArround() > 0
+					&& cells[x][y + 1].getBombArround() > 0;
+			if ((x - 1) > 0) {
+				upperLeftCorner = cells[x][y + 1].getBombArround() > 0 && cells[x - 1][y + 1].getBombArround() > 0
+						&& cells[x - 1][y].getBombArround() > 0;
+			}
+			if ((x - 1) > 0 && (y - 1) > 0) {
+				bottomLeftCorner = cells[x - 1][y].getBombArround() > 0 && cells[x - 1][y - 1].getBombArround() > 0
+						&& cells[x][y - 1].getBombArround() > 0;
+			}
+		}
+		if (bottomRightCorner || upperRightСorner || upperLeftCorner || bottomLeftCorner) {
+			predicat1 = true;
+		}
+
+		return predicat1;
+	}
+
+	public void setProbability(Cell cell) {
+		int x = cell.getPositionX();
+		int y = cell.getPositionY();
+		if (isPredicat1(cell)) {
+			probabilities[x][y] = 1f;
 		}
 	}
 
@@ -97,68 +235,6 @@ public class BotController {
 			}
 		}
 		return neibourCells;
-	}
-
-	/**
-	 * does random click on board
-	 * 
-	 * @return clicked cell
-	 */
-	public Cell doRandomClick() {
-		Random random = new Random();
-		int randomY = random.nextInt(board.getBoardWidth());
-		int randomX = random.nextInt(board.getBoardHeight());
-		Cell cell = cells[randomX][randomY];
-		Cell openedCell = cell.doOpen();
-		return openedCell;
-	}
-
-	public void doNextClick() {
-
-		for (Cell[] row : cells) {
-			for (Cell cell : row) {
-				if (!cell.isNeedsToOpen()) {
-					setProbability(cell);
-				}
-			}
-		}
-		for (int i = 0; i < board.getBoardWidth(); i++) {
-			for (int j = 0; j < board.getBoardHeight(); j++) {
-				if (probabilities[i][j] != null) {
-					if (probabilities[i][j] == 1f) {
-						cells[i][j].doFlag();
-					}
-				}
-			}
-		}
-	}
-
-	public boolean isPredicat1(Cell cell) {
-		boolean predicat1 = false;
-		int x = cell.getPositionX();
-		int y = cell.getPositionY();
-		if ((x + 1) < board.getBoardWidth() && (y + 1) < board.getBoardHeight() && (x - 1) > 0 && (y - 1) > 0) {
-			boolean bottomRightCorner = (cells[x + 1][y].getBombArround() > 0)
-					&& (cells[x + 1][y - 1].getBombArround() > 0) && cells[x][y - 1].getBombArround() > 0;
-			boolean upperRightСorner = cells[x + 1][y].getBombArround() > 0 && cells[x + 1][y + 1].getBombArround() > 0
-					&& cells[x][y + 1].getBombArround() > 0;
-			boolean upperLeftCorner = cells[x][y + 1].getBombArround() > 0 && cells[x - 1][y + 1].getBombArround() > 0
-					&& cells[x - 1][y].getBombArround() > 0;
-			boolean bottomLeftCorner = cells[x - 1][y].getBombArround() > 0 && cells[x - 1][y - 1].getBombArround() > 0
-					&& cells[x][y - 1].getBombArround() > 0;
-			if (bottomRightCorner || upperRightСorner || upperLeftCorner || bottomLeftCorner) {
-				predicat1 = true;
-			}
-		}
-		return predicat1;
-	}
-
-	public void setProbability(Cell cell) {
-		int x = cell.getPositionX();
-		int y = cell.getPositionY();
-		if (isPredicat1(cell)) {
-			probabilities[x][y] = 1f;
-		}
 	}
 
 }
